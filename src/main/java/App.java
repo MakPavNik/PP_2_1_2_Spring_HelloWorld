cf2beaa13557a14aa58333c77982ed786312412b
ASUS <pavelmakarov@@mail.com>
2022-05-17 21:22:05 +0300

    Добавлен Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat catbean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catbean.getColor());
+
+        Cat catbean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catbean1.getColor());
+
+        System.out.println(bean==bean1);
+        System.out.println(catbean==catbean1);
     }
 }
